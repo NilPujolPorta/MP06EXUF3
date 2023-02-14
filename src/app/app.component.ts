5c8ca68ab4f5c00ca50f6fd0a96171f8d824485b
Nil Pujol <nilpujolporta@gmail.com>
2023-02-14 19:34:38 +0100

    taules mes o menys funcionals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,30 +7,59 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MP06EXUF3';
-  imageSize: string = "200";
-  ngOnInit() {
-    let artworks = this.getArtworks();
-    artworks.forEach((row: any[]) => {
-      let id = row[0];
-      let title = row[1];
-      let image_id = row[2];
-      let artist_id = row[3];
-      let image = "https://www.artic.edu/iiif/2/" + image_id + "/full/" + this.imageSize + ",/0/default.jpg";
-      let artist = "https://www.artic.edu/artists/" + artist_id;
-      //let html = "<div class='card' style='width: 18rem;'><img src='" + image + "' class='card-img-top' alt='...'><div class='card-body'><h5 class='card-title'>" + title + "</h5><p class='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p><a href='" + link + "' class='btn btn-primary'>Go somewhere</a></div></div>";
-      //document.getElementById("artworks")!.innerHTML += html;
-      document.write("<table><tr><td><img src='" + image + "'></td><td>" + artist_id + "</td><td>" + title + "</td></tr></table>");
-    });
+  imageSize: string = "15";
+  limit: string = "12";
+  page: string = "1";
+  artworks: any[][] = [];
+  staticArtworks: any[][] = [];
+  async ngOnInit() {
+    this.imageSize = "15";
+    this.limit = "12";
+    this.page = "1"
+    await this.getArtworks();
+    await this.getStaticArtworks();
   }
 
-  getArtworks() {
+  async getArtworks() {
     let obres: any[][] = []
-    fetch("https://api.artic.edu/api/v1/artworks?page=1&limit=2")
+    await fetch("https://api.artic.edu/api/v1/artworks?page=" + this.page + "&limit" + this.limit)
       .then(response => response.text())
-      .then(data => JSON.parse(data).data.forEach((row: { id: any; title: any; image_id: any; artist_id: any; }) => {
+      .then(data => JSON.parse(data).data.forEach((row: { id: any; title: any; image_id: any; artist_id: any; birth: any; death: any; }) => {
         obres.push([row.id, row.title, row.image_id, row.artist_id])
       }))
       .catch(error => console.error(error));
-    return obres
+    this.artworks = obres;
+    this.showArtworks();
+  }
+  async getStaticArtworks() {
+    let obres: any[][] = []
+    await fetch("https://api.artic.edu/api/v1/artworks?page=1&limit12")
+      .then(response => response.text())
+      .then(data => JSON.parse(data).data.forEach(async (row: { id: any; title: any; image_id: any; artist_id: any; birth: any; death: any; }) => {
+        row.image_id = "https://www.artic.edu/iiif/2/" + row.image_id + "/full/" + this.imageSize + ",/0/default.jpg";
+        await fetch("https://api.artic.edu/api/v1/artists/" + row.artist_id)
+          .then(response => response.text())
+          .then(data => row.artist_id = JSON.parse(data).data.birth_date + " - " + JSON.parse(data).data.death_date + "-" + JSON.parse(data).data.title)
+          .catch(error => console.error(error));
+        console.log(row.artist_id)
+        row.birth = row.artist_id.split("-")[0] == null ? "" : row.artist_id.split("-")[0];
+        row.death = row.artist_id.split("-")[1] == null ? "" : row.artist_id.split("-")[1];;
+        row.artist_id = row.artist_id.split("-")[2];
+        obres.push([row.id, row.title, row.image_id, row.artist_id, row.artist_id, row.birth, row.death])
+      }))
+      .catch(error => console.error(error));
+    this.staticArtworks = obres;
+  }
+  showArtworks() {
+    this.artworks.forEach((row: any[]) => {
+      if (row[2] == null) {
+        row[2] = "";
+      } else {
+        row[2] = "https://www.artic.edu/iiif/2/" + row[2] + "/full/15,/0/default.jpg";
+      }
+
+      let artist_id = row[3];
+      let artist = "https://www.artic.edu/artists/" + artist_id;
+    });
   }
 }
