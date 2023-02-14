d9573b431ab350002fe8645e76dda838fa13f235
Nil Pujol <nilpujolporta@gmail.com>
2023-02-14 18:01:56 +0100

    initial cleanup and api test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MP06EXUF3';
+  ngOnInit() {
+    fetch("https://api.artic.edu/api/v1/artworks?page=1&limit=1")
+      .then(response => response.text())
+      .then(data => console.log(JSON.parse(data).data[0].artist_id))
+      .catch(error => console.error(error));
+  }
 }
